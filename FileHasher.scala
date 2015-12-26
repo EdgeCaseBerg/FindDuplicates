916476475947c0c362060ce926ab49cd31e4cd65
Ethan Joachim Eldridge <ejayeldridge@gmail.com>
2015-12-26 09:06:00 -0500

    FileHash equals method
    For convenience of being able to just do == and compare the string hash.

--- FileHasher.scala
+++ FileHasher.scala
@@ -4,7 +4,12 @@ import java.nio.file._
 import java.security.MessageDigest
 import javax.xml.bind.DatatypeConverter
 
-case class FileHash(bytes: Array[Byte], hashString: String)
+case class FileHash(bytes: Array[Byte], hashString: String) {
+		override def equals(obj: Any) = obj match {
+			case that: FileHash => this.hashString == that.hashString
+			case _ => false
+		}
+}
 
 object FileHash {
 	def apply(fPath: String) : FileHash = {
