921eb99d177df34736199da819d1588a0d7e6510
Ethan Joachim Eldridge <ejayeldridge@gmail.com>
2015-12-26 08:59:37 -0500

    Create FileHash case class
    The equality on the FileHash doesn't work of course though because bytes
    are arrays.

---
+++ FileHasher.scala
@@ -0,0 +1,18 @@
+package com.github.edgecaseberg
+
+import java.nio.file._
+import java.security.MessageDigest
+import javax.xml.bind.DatatypeConverter
+
+case class FileHash(bytes: Array[Byte], hashString: String)
+
+object FileHash {
+	def apply(fPath: String) : FileHash = {
+		val path = Paths.get(fPath)
+		val messageDigest = MessageDigest.getInstance("MD5")
+		messageDigest.update(Files.readAllBytes(path))
+		val digest = messageDigest.digest()
+		val stringHash = DatatypeConverter.printHexBinary(digest)
+		FileHash(digest, stringHash)
+	}
+}
